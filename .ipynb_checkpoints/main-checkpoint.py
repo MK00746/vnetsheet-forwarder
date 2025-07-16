55f0aaf19d798afed7c30dffbe9c4cb61711fca5
MK00746 <S65888@ocean.umt.edu.my>
2025-07-16 12:12:21 +0800

    Initial commit with Flask forwarder

---
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import requests
+
+app = Flask(__name__)
+
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxqKVaJzr0xWgZCk_Awf1EqmpO3O6-I4d7gXXFJ3kch-vZ8Al9xl2uKdVcBuaUiHKCK/exec"
+
+@app.route('/forward', methods=['POST'])
+def forward():
+    data = request.get_json()
+    print("Received:", data)
+    try:
+        response = requests.post(GOOGLE_SCRIPT_URL, json=data)
+        return {"status": response.status_code, "msg": response.text}
+    except Exception as e:
+        return {"error": str(e)}, 500
+
+@app.route('/', methods=['GET'])
+def home():
+    return "V-NET Gateway is running.", 200
